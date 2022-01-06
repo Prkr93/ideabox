241c2719a6475d806ed3e08657bf330dee37cbb6
Matthew Meier <mmeier211@gmail>
2022-01-06 12:05:12 -0600

    Write function that captures input

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ var saveButton = document.querySelector('.save-button');
 var titleTextField = document.querySelector('#title-text-field');
 var bodyTextField = document.querySelector('#body-text-field');
 var ideas = [];
+
+saveButton.addEventListener('click', myFunction);
+
+function myFunction(e){
+  e.preventDefault();
+  if(titleTextField.value && bodyTextField.value) {
+    ideas.push(new Idea(titleTextField.value, bodyTextField.value));
+  }
+}

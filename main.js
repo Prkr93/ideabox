fd89416e8aadba00ebc8cdb7fd4b0ed4605185a1
Matthew Meier <mmeier211@gmail>
2022-01-06 11:53:59 -0600

    Add query selectors to the form elements

---
+++ main.js
@@ -0,0 +1,4 @@
+var saveButton = document.querySelector('.save-button');
+var titleTextField = document.querySelector('#title-text-field');
+var bodyTextField = document.querySelector('#body-text-field');
+var ideas = [];
